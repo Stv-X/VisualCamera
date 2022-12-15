58693f460820c02775399a8b2d712017b8e6a499
Stv.X <xsm_2001@163.com>
2022-12-15 18:44:51 +0800

    Initial Commit

---
+++ VisualCamera/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisualCamera
+//
+//  Created by 徐嗣苗 on 2022/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
